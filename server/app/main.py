971073c19f728aacba120906721f86aee8d5d69f
Lucas Jensen <lucas.p.jensen10@gmail.com>
2024-05-03 13:46:01 -0700

    fixed user route registration

--- server/app/main.py
+++ server/app/main.py
@@ -9,6 +9,7 @@ from app.routers.contact import router as contact_router
 from app.routers.events import router as event_router
 from app.routers.group import router as group_router
 from app.routers.musicians import router as musician_router
+from app.routers.users import router as user_router
 from app.scripts.version import get_version
 
 app = FastAPI(
@@ -20,6 +21,7 @@ app.include_router(musician_router)
 app.include_router(group_router)
 app.include_router(contact_router)
 app.include_router(event_router)
+app.include_router(user_router)
 
 controller = MainController()
 
--- server/app/routers/users.py
+++ server/app/routers/users.py
@@ -12,15 +12,6 @@ router = APIRouter(
 )
 
 
-"""
-Note: this router is not currently registered in the main FastAPI app.
-This is to facilitate not exposing the user routes to the public API,
-but this may change in a future version.
-
-The file remains to ease the future addition of user routes.
-"""
-
-
 @router.get("/", status_code=status.HTTP_200_OK)
 async def get_users() -> list[User]:
     return await controller.get_users()
